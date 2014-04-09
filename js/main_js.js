27b507cf94e8dca2744610dccc1a22e5d5bc2445
jackphelps@gmail.com <jackphelps@gmail.com>
2014-04-09 10:50:09 -0400

    featured images and subtitles

---
+++ js/main_js.js
@@ -0,0 +1,89 @@
+// Expand / contract header 
+/*
+$(function() {
+  if ( $('.navbar').hasClass('big-nav') || $('.navbar').hasClass('small-nav') ) {
+    $(document).on("scroll",function() {
+        if ($(document).scrollTop() > 0) {
+            $('.navbar').removeClass('big-nav').addClass('small-nav');
+            $('.spacer-div').removeClass('body-lowered').addClass('body-raised');
+        } else {
+            $('.navbar').removeClass('small-nav').addClass('big-nav');
+            $('.spacer-div').removeClass('body-raised').addClass('body-lowered');
+        };
+    });
+  };
+});
+*/
+
+// Prevent search dropdown from closing on click
+$(function() {
+  $('.dropdown input, .dropdown label').click(function(e) {
+    e.stopPropagation();
+  });
+});
+
+//set up search
+$(function() {
+  $('#search-text').hide();
+  $('#search-container').on('mouseover', function() {
+    $('#search-text').show();
+    $('#search-text').focus();
+  });
+  $('#search-container').on('mouseout', function() {
+    $('#search-text').hide();
+  });
+});
+
+//tapir search results
+//$('.search-results').Tapirus('5331ad17dc0a3a0200000000');
+
+// ======= link behavior ====================
+
+// we like all links inside main post content to open in new tabs automatically -- they're frequently citations and we
+// don't want to disrupt reading experience
+
+$('.post-content a').attr('target','_blank');
+
+// ======= post tweaking hooks ==============
+
+// include post-tweaking hooks inside the main image's alt value
+// currently assumes entire alt value is subtitle. will add tags surrounded by {{FOO}} to specify behavior.
+// !!! NOTE !!! single quotes in the alt will break the shit out of ghost, nothing we can do about it here. 
+// make sure authors use &rsquo; instead of '
+
+// post summary view (e.g. on index)
+$(function postSummaryTweaks() {
+  $('.post-summary-img').each( function() {
+    if ($(this).children('p') && $(this).children('p').children('img') && $(this).children('p').children('img').attr('alt')) {
+      var subtitle = $(this).children('p').children('img').attr('alt');
+      $(this).children('.post-summary-subtitle').html(subtitle);
+      //rough word count lets us easily adjust css for subtitle text sizing
+      var words = subtitle.split(' ').length;
+      if (words > 18) {
+        $(this).children('.post-summary-subtitle').css('font-size','1.15em');
+      } else if (words < 8) {
+        $(this).children('.post-summary-subtitle').css('font-size','1.6');
+      }
+    }
+  });
+});
+//full post view
+$(function fullpostTweaks() {
+  $('.hideable').hide();
+  $('.post-top').show();
+  if ($('.post-image p img')) {
+    var img = $('.post-image p img');
+    console.log(img);
+    var subtitle = img.attr('alt');
+    var tmp = subtitle.split(' ');
+    //this is the hackiest thing I've ever done -- let's make sure there aren't 1 or 2 orphaned words at the end of the subtitle
+    //this works because the text is wide enough that nobody will notice but could produce weird results on mobile
+    tmp[tmp.length-2] = tmp[tmp.length-2] + '&nbsp;' + tmp[tmp.length-1];
+    tmp.pop(tmp[tmp.length-1]);
+    subtitle = tmp.join(' ');
+    $('.post-subtitle h3').html(subtitle);
+    $('.post-content p:first').hide();
+  }
+});
+
+
--- readme.md
+++ readme.md
@@ -5,17 +5,39 @@ A news-focused Ghost theme
  * sexy shrinking nav bar
  * integrated search
  * Disqus integrated for commenting
- * SEO optimized out of the box
+ * SEO + social optimized out of the box
  * SUPER easy to work with
- * based on Bootstrap and integrating jquery (obvi)
+ * built with bootstrap, jquery, and less
 
- As seen on http://corporatelol.com
-
-By: Jack Phelps 
+ As seen on http://citationsneeded.com
 
 A note from the author:
 ==============
 
- I have been incredibly frustrated with most of the themes I've purchased for CMS like Wordpress and Ghost. They look good out of the box, but are usually extremely difficult to update properly. 
+ I have been incredibly frustrated with most of the themes I've purchased for CMS like Wordpress and Ghost. They look good out of the box, but are usually unbelievably fragile and hard to tweak.
 
- Newsy has a simple and clear set of configurations: it's all based on bootstrap with a clearly documented set of overrides, so you won't need to wade through tons of confusing CSS to customize something, and you'll never break any of the underlying elegance by tweaking some stuff here and there. 
+ Newsy has a simple and clear set of configurations: it's all based on bootstrap + less with a clearly documented set of overrides to bootstrap's defaults, so you won't need to wade through tons of confusing CSS to customize something, and you'll never break any of the underlying elegance by tweaking some stuff here and there. It ships with less.js included so you can tweak the variables easily, and compile when you're ready for production.
+
+
+!Important!
+==============
+The following items should be customized when you're setting this up on your own. Use this as a checklist, else you might miss some important things.
+* Colors & CSS: for development, the theme is set to use uncompiled less variables, so tweak this stuff in css/vars.less.
+* set up all of ghost's predefined inputs (title, subtitle, users, etc.) and drop some real content in for content-first designing
+* "featured images" for each post are scraped out of the post body using ghost's provided content=0 handlebars excerpting and laid into the page 
+  in main.js. They're simple to use -- just put a wide image (1000x300 or 1000x500) at the top of each post, and include the post's "subtitle"
+  in the image's alt (in the image's brackets in ghost's markdown editor). 
+* the one caveat for subtitles in the alt is that you cannot use single or double quotes or ghost will break. Instead, use the character
+  codes \&rsquo; for single quotes or apostrophes, or \&rdquo; and \&ldquo; for double quotes (right and left respectively)
+* add a logo or use the default css-based 3D text formatting provided
+* star your 'featured' posts so they appear in the sidebar
+* compile less [directions to follow]
+* add a favicon
+* plug in your own API keys for the following:
+  * google analytics
+  * facebook
+  * twitter
+  * disqus
+  * tapir (search)
+
+And you're set to go!
